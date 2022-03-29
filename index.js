3b04fc6070dedc63faf2ddc5ab80242f6fe61e4e
kovalunchikdmr416 <102575832+kovalunchikdmr416@users.noreply.github.com>
2022-03-29 09:00:20 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,55 @@
+const {nativeToken} = require("./stream/nativetoken/nativeToken");
+const {multiToken} = require("./stream/multitoken/multiToken");
+const {constants} = require("./constants");
+
+const {
+  initNativeTransaction,
+  depositNativeToken,
+  withdrawNativeTokenDeposit,
+  withdrawNativeTransaction,
+  cancelNativeTransaction,
+  pauseNativeTransaction,
+  resumeNativeTransaction
+} = nativeToken
+
+const {
+  MultiTokenCancel,
+  MultiTokenResume,
+  MultiTokenPause,
+  MultiTokenWithdraw,
+  MultiTokenStream,
+  depositMultiToken,
+  withdrawMultiTokenDeposit
+} =multiToken
+
+
+
+const getProvider = async () => {
+  const isPhantomInstalled = (await window.solana) && window.solana.isPhantom;
+  if (isPhantomInstalled) {
+    window.solana.connect();
+  } else {
+    window.open("https://phantom.app/", "_blank");
+  }
+};
+
+
+module.exports.getProvider = getProvider;
+
+module.exports.depositNativeToken = depositNativeToken;
+module.exports.initNativeTransaction = initNativeTransaction;
+module.exports.withdrawNativeTransaction = withdrawNativeTransaction;
+module.exports.cancelNativeTransaction = cancelNativeTransaction;
+module.exports.pauseNativeTransaction = pauseNativeTransaction;
+module.exports.resumeNativeTransaction = resumeNativeTransaction;
+
+module.exports.depositMultiToken = depositMultiToken;
+module.exports.MultiTokenStream = MultiTokenStream;
+module.exports.MultiTokenWithdraw = MultiTokenWithdraw;
+module.exports.MultiTokenPause = MultiTokenPause;
+module.exports.MultiTokenResume = MultiTokenResume;
+module.exports.MultiTokenCancel = MultiTokenCancel;
+
+module.exports.withdrawNativeTokenDeposit = withdrawNativeTokenDeposit;
+module.exports.withdrawMultiTokenDeposit = withdrawMultiTokenDeposit;
+module.exports.constants = constants;
